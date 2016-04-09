9d03d1b7a614d16db001b7cd7eab2f174f0984e6
Lavan <Lavan@MacBook-Pro-de-Lavan.local>
2016-04-10 00:01:49 +0200

    dqz

--- inc/db.php
+++
@@ -1,7 +0,0 @@
-<?php  
-	//Connexion a la BASE DE DONNEES AVEC PDO
-	$pdo= new PDO('mysql:dbname=Costumeo;host=127.0.0.1', 'root', 'Chopper1697');
-
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO:: FETCH_OBJ);
